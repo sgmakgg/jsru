0480bb670360e9a5c63756215ac262bab82fc2f4
Makarov Sergey <sgmakgg@gmail.com>
2024-09-24 15:41:44 +0300

    HW3

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,11 +1,6 @@
-import { createRoot } from 'react-dom/client'
-import {restaurants} from "./constants/materials/mock.js";
-import {App} from "./components/app/App.jsx";
+import { createRoot } from 'react-dom/client';
+import { restaurants } from './constants/materials/mock.js';
+import { App } from './components/app/App.jsx';
 
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-    <App topic="Restaurants" restaurants={restaurants} />
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<App topic="Restaurants" restaurants={restaurants} />);
