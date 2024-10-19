7f7452f466053edd9319809e6ce625b45bf7d743
Sergey Makarov <sgmakgg@gmail.com>
2024-10-19 21:37:50 +0300

    HW 9

--- react/src/components/reviews/Reviews.jsx
+++ react/src/components/reviews/Reviews.jsx
@@ -1,11 +1,10 @@
 import { restaurantsSelectors } from "../../redux/entities/restaurants/restaurantsSlice.js";
 import { Review } from "../review/review.jsx";
-import { store } from "../../redux/store.js";
+import { useSelector } from "react-redux";
 
 export const Reviews = ({ restaurantId }) => {
-  const restaurant = restaurantsSelectors.selectById(
-    store.getState(),
-    restaurantId,
+  const restaurant = useSelector((state) =>
+    restaurantsSelectors.selectById(state, restaurantId),
   );
 
   return (
